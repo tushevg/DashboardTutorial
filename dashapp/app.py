d3e60722a08619272235a9c52215a93950b187e2
Simon Thomsen <simon.thomsen@outlook.de>
2021-08-23 17:13:17 +0200

    update app.py

--- dashapp/app.py
+++ dashapp/app.py
@@ -13,7 +13,23 @@ app = dash.Dash(__name__)
 #Define the app
 app.layout = html.Div()
 
-#Run the app 
+#Run the app ($ python app.py)
 if __name__ == '__main__':
     app.run_server(debug=True)
 
+#app layout
+app.layout = html.Div(children=[
+    html.Div(className='row', #Define the row element
+        children=[
+            html.Div(className='four columns div-user-controls'), #Define left element
+
+            html.Div(className='eight columns div-for-charts bg-grey') #Define right element
+
+        ])
+])
+
+children = [
+    html.H2('Dashboard for Stockprises'),
+    html.P('Visualizing time series with Plotly - Dash'),
+    html.P('Pick one or more stocks from the dropdown below.')
+]
