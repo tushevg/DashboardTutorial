becc8b395180f2dfe20816d7a74083a579a1ee53
Simon Thomsen <simon.thomsen@outlook.de>
2021-08-26 16:49:36 +0200

    update app.py

--- dashapp/app.py
+++ dashapp/app.py
@@ -1,35 +1,33 @@
 import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 
 #Load Data
 df = pd.read_csv('data/stockdata2.csv', index_col=0, parse_dates=True)
 df.index = pd.to_datetime(df['Date'])
 
-import dash
-import dash_html_components as html
-
 #Initialise the app
 app = dash.Dash(__name__)
 
 #Define the app
-app.layout = html.Div()
-
+app.layout = html.Div(
+    children=[
+        html.Div(className='row', #Define the row element
+            children=[
+                html.Div(className='four columns div-user-controls', #Define left element
+                    children=[
+                        html.H2('Dash - Stock Prices'),
+                        html.P('Visualizing time series with Plotly - Dash'),
+                        html.P('Pick one or more stocks from the dropdown below.'),
+                    ]
+                ),
+            html.Div(className='eight comlumns div-for-charts bg-grey'), #Define right element
+            
+        ])
+])
 #Run the app ($ python app.py)
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-#app layout
-app.layout = html.Div(children=[
-    html.Div(className='row', #Define the row element
-        children=[
-            html.Div(className='four columns div-user-controls'), #Define left element
-
-            html.Div(className='eight columns div-for-charts bg-grey') #Define right element
-
-        ])
-])
-
-children = [
-    html.H2('Dashboard for Stockprises'),
-    html.P('Visualizing time series with Plotly - Dash'),
-    html.P('Pick one or more stocks from the dropdown below.')
-]
